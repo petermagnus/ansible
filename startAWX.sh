3e4cebb7fb0e05821d93b180170f7882dd8af58f
Peter Magnus Prestesæter <pmp@marcello.no>
2021-03-04 15:02:21 +0100

    test

---
+++ startAWX.sh
@@ -0,0 +1,5 @@
+#pip install docker ansible docker-compose
+
+# https://docs.ansible.com/ansible/devel/dev_guide/developing_modules_general.html#environment-setup
+
+ansible-playbook -i inventory install.yml -vv
